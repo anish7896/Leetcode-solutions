c7a74777842b09ab98b1f58f6b3d20a3dd6f360e
Anish <anishgupta1789@gmail.com>
2026-06-27 14:28:20 +0530

    Time: 6 ms (74.27%), Space: 8 MB (92.48%) - LeetHub

---
+++ 2376-count-special-integers/2376-count-special-integers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dp[10][2][1024][2];
+    int solve(string &s, int idx, int tight, int mask, int started){
+        if(idx==s.size()) return started;
+        if(dp[idx][tight][mask][started]!=-1) return dp[idx][tight][mask][started];
+        int limit = (tight == 1) ? s[idx]-'0' : 9;
+        int ans = 0;
+        for(int i=0;i<=limit;i++){
+            if(started == 0 && i==0){
+                ans += solve(s, idx+1, (tight && (0==limit)), mask, 0);
+            }
+            else{
+                if(mask & (1<<i)) continue;
+                ans += solve(s, idx+1, (tight && (i==limit)), (mask | (1<<i)), 1);
+            }
+        }
+        return dp[idx][tight][mask][started] = ans;
+    }
+    int countSpecialNumbers(int n) {
+        string s = to_string(n);
+        memset(dp, -1, sizeof(dp));
+        return solve(s, 0, 1, 0, 0);
+    }
+};
